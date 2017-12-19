50cc1d6ca50891d07d597a63f3943b55b03f6e2f
osama miro <o.miro650@gmail.com>
2017-12-19 02:46:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(words)# code an argument here
 end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
 
   cheese_types = ["cheddar", "gouda", "camembert"]
