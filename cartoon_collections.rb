7f3c55af16cec9b9df58f085ff56a2451537d897
osama miro <o.miro650@gmail.com>
2017-12-19 02:02:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(arr)# code an argument here
   # Your code here
-  dwarfs.each_with_index do | name , index |
+  arr.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
 end
