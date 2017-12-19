3c45a1f1093bfe82dbb6c4d82a0005726880de45
osama miro <o.miro650@gmail.com>
2017-12-19 02:09:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(arr)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)# code an argument here
   # Your code here
+  arr.collect do |x|
+  "#{x.upcase}!"
 end
 
 def long_planeteer_calls# code an argument here
