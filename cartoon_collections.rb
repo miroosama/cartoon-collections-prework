ca588a484bf5f305f589ea4e829dfb42a1e10e16
osama miro <o.miro650@gmail.com>
2017-12-19 02:46:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,10 @@ end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+ cheese.find do |cheese|
+   cheese_types.include?(cheese)
+ end
+end
 end
